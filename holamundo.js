41c2f58d976783730c3e1b216984ff4448495f72
alexisMJI <alexismarin11@hotmail.es>
2021-08-15 18:09:18 -0300

    confirmo10

--- holamundo.js
+++ holamundo.js
@@ -1,29 +1,37 @@
 'use strict'
 /*
 Esto es un comentario
-Esto es un comentario nuevos
-nuevo coment 333
 */
-//esto tambien es un comentario
 
 alert("Hola mundo con JavaScriptt");
 document.write("esto esta escrito desde JS");
 console.log("asdas aparece en la consola");
 
-//-----------------------------
-var pais ="España";
-var año =1999;
 //let permite declarar variables limitando su alxance a el bloque donde se usa
 //el var se utiliza de manera mas global
-let prueba="sadasd";
-let ancho=88;
+
+var prueba="estos son caracteres";
+var ancho=88;
+var pais ="España";
+var año =1999;
 
 console.log(pais,año);
 
 console.log(prueba,ancho);
 
-año= 1999-2021;
+if(true){
+    let prueba="en LET"
+    let ancho=2323;
+    console.log(prueba,ancho);
+}
 
-año= 12;
-//comentariosandkjaskdj
-//kajsdhkajhdksad
+console.log(prueba,ancho);
+
+
+año= 2021;
+console.log(año);
+alert("El año presente al momento de tipear esto es "+ año);
+//variables y operadores
+
+
+
