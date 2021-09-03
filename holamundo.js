f2ce69cd59df257ba90f8730c774c6bb8f0b7e2a
alexisMJI <alexismarin11@hotmail.es>
2021-09-03 00:21:52 -0300

    confirmo 18

--- holamundo.js
+++ holamundo.js
@@ -48,7 +48,7 @@ for (let i = 0; i <= 20; i++){
 //--------------------------kmjhkjh
 //alertas y ventanas para ingreso de datos
 alert("Hola mundo con JavaScriptt");
-
+/*
 var booleann =confirm("quieres poner verdadero o falso?");
 
 if(booleann == true)
@@ -61,14 +61,52 @@ console.log("el nombre del usuario es "+nombreUsuario);
 
 var numeroIngresado = parseInt(prompt("ingresa un numero"));
 console.log(numeroIngresado+2);
+*/
 
-//---------------------------modificar cosas de html
-//var cajita=document.getElementById("micaja").innerHTML;
-var cajita= document.querySelector('#micaja');
+function cambiaColor(color){
+    cajita.innerHTML = '<h3>Hola que tal</h3>';
+    cajita.style.background= "color";
+}
+
+/*DOM document object model
+modificar cosas de html
+Aca vemos como seleccionar un elemetno de html en js el primero es con su id*/
+var cajita=document.getElementById("micaja");
+//var cajita= document.querySelector('#micaja');
 console.log(cajita);
+//aca al div de html le vamos a poner que imprima algo
+cajita.innerHTML = '<h3>Hola que tal</h3>';
+cajita.style.background= "blue";
 
-document.getElementById('micaja').innerHTML = '<h3>Hola que tal</h3>';
+//BOM browser object model
+console.log(screen.width);
+console.log(window.location);
 
-//seguimos con los cambios de html sobre js
+//window.open("https://www.google.com");
+
+// ahora vemos como capturar eventos
 
 
+var cont =0;
+function contadorr(){
+    
+    cont= cont +1;
+    console.log(cont);
+}
+
+function vercontador(){
+    console.log=cont;
+}
+
+/*var botoncito2 = document.getElementById(button2);
+botoncito2.addEventListener("click",function contadorrr(){
+    
+    cont= cont +1;
+    console.log(cont);
+});*/
+
+//--
+document.getElementById( "try" ).addEventListener( "click" , function ()
+{ 
+    cont= cont +1;
+    console.log(cont);}); 
