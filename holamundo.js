be4ca522cd7e3ec2e614badd4d3873487f5f3641
alexisMJI <alexismarin11@hotmail.es>
2021-09-04 00:05:12 -0300

    confirm 19

--- holamundo.js
+++ holamundo.js
@@ -98,15 +98,82 @@ function vercontador(){
     console.log=cont;
 }
 
-/*var botoncito2 = document.getElementById(button2);
-botoncito2.addEventListener("click",function contadorrr(){
-    
-    cont= cont +1;
-    console.log(cont);
-});*/
 
-//--
+
+//--tambien tenemos clickover y clicout o clickdb o algo asi
 document.getElementById( "try" ).addEventListener( "click" , function ()
 { 
     cont= cont +1;
     console.log(cont);}); 
+
+
+/* eventos teclado
+FOCUS
+BLUR
+KEYDOWN
+KEYUP
+*/
+
+var botonform1;
+botonform1= document.getElementById( "btnform" );
+botonform1.addEventListener( "click" , function ()
+{ 
+    cont= cont +1;
+    console.log(cont);}); 
+
+var inputt1 =document.getElementById( "input1" );
+
+inputt1.addEventListener( "focus" , function ()
+{ 
+    console.log("[focus]Estas dentro del input");});
+
+
+inputt1.addEventListener( "blur" , function ()
+    { 
+        console.log("[blur]Estas fuera del input");});
+    
+inputt1.addEventListener( "keydown" , function ()
+    { 
+    console.log("[keydown]Estas escribiendo dentro del input");});   
+
+inputt1.addEventListener( "keypress" , function (event)
+    { 
+    console.log("[keypress]tecla presionada",String.fromCharCode(event.AT_TARGET));});   
+
+/*
+Otro tema que se ve es que podemos poner nuestro script en html al principio
+y para que funcione bien, vamos a poner window.addeventlistener("load" function (){tdo el codigo que creamos}
+sirve para ejecutar el codigo cuando todo el html este cargado
+*/
+ //Otro tema es los timers
+/*
+ var tiempo= setInterval( function(){
+    console.log("Set interval ejecutado");
+}, 3000);
+
+var botontime= document.getElementById( "btntime" );
+botontime.addEventListener( "click", function()
+{ 
+    console.log("has parado el bucle time");
+clearInterval(tiempo);
+}); 
+*/
+
+/*otro tema es lo del evento submit en los forms
+basicamente es un input type""submit"
+y sirve para poner una especie de boton que va a 
+servir para enviar la info del form
+
+esta en la seccion 19 de los videos
+AHORA LO VEMOS UN POCO
+*/
+
+var formulario= document.getElementById("form1");
+formulario.addEventListener("submit", function(){
+    console.log("se ejecuto el submit");
+    var nombree= document.getElementById("nombreee").value;
+    console.log(nombree);
+    return false;
+
+})
+
