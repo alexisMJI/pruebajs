0ab1df4e9222762ac82560d51e3281ef0d158ee2
alexisMJI <alexismarin11@hotmail.es>
2022-03-09 13:36:39 -0300

    m

--- JS/JQUI.js
+++ JS/JQUI.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){  
     console.log("La web y Jquery cargados..");
 
+    /*hay bastantes metodos nuevos de interacciones con jqui
+    vamos a ver los q me iwnteresen */
 
+    //Selectable
+    $(".listaSel").selectable();
+    
+    //tooltip
+    $('a').tooltip();
+    //cuadros de dialogos
+    $("#btnl").click(function(){
+        $("#cajaPopup").dialog();
+    })
+    
+    //datepicker o calendario...
+    $("#iCalend").datepicker();
 
-
+    $("#divtabs").tabs();
 
 
 
